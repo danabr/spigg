6f30609fde98a496d69ee0a9524fe5b9d61daae8
Daniel Abrahamsson <hamsson@gmail.com>
2016-04-11 07:30:16 +0200

    Include fixture

---
+++ test/fixtures/pure.erl
@@ -0,0 +1,16 @@
+-module(pure).
+
+add(A, B) when is_integer(A), is_integer(B) ->
+  A + B.
+
+reverse(L) when is_list(L) ->
+  lists:reverse(L).
+
+even(0)            -> true;
+even(N) when N > 0 -> not odd(N-1).
+
+odd(1)            -> true;
+odd(N) when N > 0 -> not even(N-1).
+
+sum([])       -> 0;
+sum([X|Rest]) -> X + sum(Rest).
