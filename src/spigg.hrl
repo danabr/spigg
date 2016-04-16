498fd41570ddbde4f4af6cf88f4f83e739175612
Daniel Abrahamsson <hamsson@gmail.com>
2016-04-16 11:14:43 +0200

    Include a version number in the db record.

--- src/spigg.hrl
+++ src/spigg.hrl
@@ -1,7 +1,8 @@
 -ifndef(__SPIGG_HRL).
 -define(__SPIGG_HRL, true).
 
--record(db, { functions = #{} :: #{mfa() => spigg:func()}
+-record(db, { version=1 :: non_neg_integer()
+            , functions = #{} :: #{mfa() => spigg:func()}
             }).
 
 -record(function, { native_side_effects = [] ::
